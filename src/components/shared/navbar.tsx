49812447d8b0851cad88c15022fb24ad745e15c5
Vaishnavi <vaishnavip276@gmail.com>
2025-06-10 06:40:20 +0000

    updated nav

--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -7,6 +7,7 @@ import { getPrimaryname } from "@/lib/utils";
 import { Copy, LogOut, User, UserIcon, Wallet } from "lucide-react";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { toast } from "sonner";
+import { url } from "inspector";
 
 export default function Navbar() {
     //@ts-ignore
@@ -82,6 +83,10 @@ export default function Navbar() {
         {
             name: "History",
             url: `/deployment/history?repo=${extractRepoName(repo || "")}`,
+        },
+        {
+            name: "Branch Status",
+            url: `/deployment/branch-status?repo=${extractRepoName(repo || "")}`,
         },
         {
             name: "Logs",
